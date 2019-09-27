9b8076df9d53c7dc968cc4f66463b7bbd77d29b0
Luke Davies <lkdvs3@gmail.com>
2019-09-27 21:26:02 +0100

    Updated check script

--- .github/scripts/check-clean.sh
+++ .github/scripts/check-clean.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-if [ -z "$(git status -s)" ]; then
+STATUS=$(git status -s)
+if [ -z "$STATUS" ]; then
+  echo "Action build up to date"
   exit 0
 else
+  echo "Action build not up to date"
+  echo "$STATUS"
   exit 1
 fi
